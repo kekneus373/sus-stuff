d95da6bcb4ab984dd8e47a0a699b62aa59af9e5d
kekneus373 <klu4k0@protonmail.com>
2026-01-01 12:38:19 +0200

    Delete retouchpad.bash
    moved to my gist

--- retouchpad.bash
+++
@@ -1,6 +0,0 @@
-#!/usr/bin/env bash
-echo "Disabling touchpad..."
-sudo modprobe -r psmouse
-echo "Enabling touchpad..."
-sudo modprobe psmouse
-echo "Should work now. Otherwise, check 'dmesg' or reboot"
