ca4d6e7c5b37a2ac14081e47e768b0450a9fb2a6
kekneus373 <klu4k0@protonmail.com>
2026-06-14 22:34:10 +0300

    Update 9p.bash
    new polished version

--- fedora/9p.bash
+++ fedora/9p.bash
@@ -2,11 +2,21 @@
 
 # Re-run the script as root if not already running as root
 if [ "$EUID" -ne 0 ]; then
-  echo "This script requires root. Re-running with sudo..."
-  exec sudo "$0" "$@"
+        echo "[$(date +'%Y-%m-%d %H:%M:%S')] This script requires root. Re-running with sudo..."
+        exec sudo "$0" "$@"
 fi
 
 tag="/mnt/sh"
 mount="/mnt/sh"
 
-mount -t 9p -o trans=virtio,mount_tag="$tag",rw,users "$tag" "$mount"
+echo "--- Script for connecting the shared 9p VirtIO volume ---"
+echo ">>> Choose action:"
+select profile in "Mount \`$mount\`" "Unmount \`$mount\`"; do
+    case $REPLY in
+        1) mount -t 9p -o trans=virtio,mount_tag="$tag",rw,users,x-gvfs-show "$tag" "$mount"; break ;;
+        2) umount "$tag"; break ;;
+        *) echo "[$(date +'%Y-%m-%d %H:%M:%S')] Invalid option." ;;
+    esac
+done
+
+exit
